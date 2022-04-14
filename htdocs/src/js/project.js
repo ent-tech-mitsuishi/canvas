00b1cd273636387a1254c323cc69cd26a7a2aee9
ent-tech-mitsuishi <mitsuishi@entacl.com>
2022-04-14 16:54:09 +0900

    途中まで

--- htdocs/src/js/project.js
+++ htdocs/src/js/project.js
@@ -1,1 +1,21 @@
-console.log("start");
+function draw() {
+  var canvas = document.getElementById("canvas");
+  if (canvas.getContext) {
+    var ctx = canvas.getContext("2d");
+
+    // 三次ベジェ曲線の例
+    ctx.beginPath();
+    ctx.moveTo(75, 40);
+    ctx.bezierCurveTo(75, 37, 70, 25, 50, 25);
+    ctx.bezierCurveTo(20, 25, 20, 62.5, 20, 62.5);
+    ctx.bezierCurveTo(20, 80, 40, 102, 75, 120);
+    ctx.bezierCurveTo(110, 102, 130, 80, 130, 62.5);
+    ctx.bezierCurveTo(130, 62.5, 130, 25, 100, 25);
+    ctx.bezierCurveTo(85, 25, 75, 37, 75, 40);
+    ctx.fill();
+  }
+}
+
+$(window).on("load", function () {
+  draw();
+});
